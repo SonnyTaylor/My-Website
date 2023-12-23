e606f196d3595ba660e3f6c960d06f395f0081a4
salaminipples <santinotaylor08@gmail.com>
2023-12-23 18:20:34 +1100

    Refactor Cat Facts.js to use import statement for localStorage

--- scripts/Cat Facts.js
+++ scripts/Cat Facts.js
@@ -1,30 +1,4 @@
-fetch("https://catfact.ninja/fact")
-  .then((response) => response.json())
-  .then((data) => {
-    const catFact = data.fact;
-    const storedFacts = localStorage.getItem("catFacts");
-    let storedFactsArray = [];
-
-    if (storedFacts) {
-      storedFactsArray = JSON.parse(storedFacts);
-    }
-
-    if (storedFactsArray.includes(catFact)) {
-      // If the fact has already been shown, fetch a new one
-      console.log("Fact already shown (${catFact})");
-      fetchNewCatFact();
-    } else {
-      // Store the new fact in local storage and display it
-      storedFactsArray.push(catFact);
-      localStorage.setItem("catFacts", JSON.stringify(storedFactsArray));
-      document.getElementById("cat-fact").textContent = catFact;
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-    document.getElementById("cat-fact").innerHTML =
-      "Error: Failed to fetch cat fact";
-  });
+import { localStorage } from 'localStorage';
 
 /**
  * Fetches a new cat fact from the API, stores it in local storage, and displays it on the webpage.
@@ -58,3 +32,6 @@ function fetchNewCatFact() {
         "Error: Failed to fetch cat fact";
     });
 }
+
+// Call the function to fetch a new cat fact
+fetchNewCatFact();
